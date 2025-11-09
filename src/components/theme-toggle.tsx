01006c302f0086d14b569206b3c5f481cd6317d4
Italo Seara <italo.sseara@gmail.com>
2025-11-09 15:54:26 -0300

    feat: i18n

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -8,12 +8,18 @@ import { Button } from "@/components/ui/button";
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
 
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+
+    if (typeof document.startViewTransition === "function") {
+      document.startViewTransition(() => setTheme(newTheme));
+    } else {
+      setTheme(newTheme);
+    }
+  };
+
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
       <Sun className="w-[1.2rem] h-[1.2rem] rotate-0 dark:-rotate-90 scale-100 dark:scale-0 transition-all" />
       <Moon className="absolute w-[1.2rem] h-[1.2rem] rotate-90 dark:rotate-0 scale-0 dark:scale-100 transition-all" />
       <span className="sr-only">Toggle theme</span>
