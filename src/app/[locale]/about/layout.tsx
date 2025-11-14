93aaa9fbae30d108767bae011da2d40566d5866d
Italo Seara <italo.sseara@gmail.com>
2025-11-14 19:48:14 -0300

    wip: routes

---
+++ src/app/[locale]/about/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+type MetadataProps = Readonly<{
+  params: Promise<{ locale: string }>;
+}>;
+
+export async function generateMetadata({ params }: MetadataProps): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "metadata.about" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
+
+export default async function AboutLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
