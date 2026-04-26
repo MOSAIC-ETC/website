054343f00d8ba4b4d33e9735ac5ecbb24b22c62d
Ítalo Seara <italo.sseara@gmail.com>
2026-04-26 19:18:55 -0300

    feat: rename compressed to preview

--- src/app/[locale]/(public)/etc/lib/objects.ts
+++ src/app/[locale]/(public)/etc/lib/objects.ts
@@ -4,7 +4,7 @@ export const OBJECTS: ObjectEntry[] = [
   {
     id: "manga-9041-6101",
     name: "MaNGA 9041-6101",
-    previewPath: "/data/objects/MaNGA-9041-6101/compressed.fits",
+    previewPath: "/data/objects/MaNGA-9041-6101/preview.fits",
     cubePath: "/data/objects/MaNGA-9041-6101/cube.fits",
   },
 ];
